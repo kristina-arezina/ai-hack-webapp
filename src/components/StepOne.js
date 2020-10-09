2c32f1c923d371b4dfa23c636dfb1b6540c3d54d
kristina-arezina <arezina.kristina23@gmail.com>
2020-10-08 21:19:17 -0400

    removed skip

--- src/components/StepOne.js
+++ src/components/StepOne.js
@@ -6,6 +6,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Instructions from "./Instructions"
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -69,7 +70,7 @@ export default function StepOne() {
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        Item One
+        <Instructions/>
       </TabPanel>
       <TabPanel value={value} index={1}>
         Item Two
