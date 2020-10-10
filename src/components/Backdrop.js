e572f571814bcca239b7dc57a2a4873db7e7679c
kristina-arezina <arezina.kristina23@gmail.com>
2020-10-09 20:38:12 -0400

    moving works

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -4,6 +4,8 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
+// NOTE: I am keeping the backdrop button trigger as a commen so we can use the functions it has that riger it
+
 const useStyles = makeStyles((theme) => ({
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
@@ -13,19 +15,19 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SimpleBackdrop() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
   const handleClose = () => {
     setOpen(false);
   };
-  const handleToggle = () => {
-    setOpen(!open);
-  };
+  // const handleToggle = () => {
+  //   setOpen(!open);
+  // };
 
   return (
+    // <Button variant="outlined" color="primary" onClick={handleToggle}>
+    //     Show backdrop
+    //   </Button>
     <div>
-      <Button variant="outlined" color="primary" onClick={handleToggle}>
-        Show backdrop
-      </Button>
       <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
         <CircularProgress color="inherit" />
       </Backdrop>
--- src/components/StepOne.js
+++ src/components/StepOne.js
@@ -71,7 +71,7 @@ export default function StepOne() {
       <AppBar style={{backgroundColor: "#80d6ff"}} position="static">
         <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
           <Tab style={{color: "#000a12"}} label="Choose Resumes" {...a11yProps(0)} />
-          <Tab style={{color: "#000a12"}} label="Item Two" {...a11yProps(1)} disabled={false} />
+          <Tab style={{color: "#000a12"}} label="Item Two" {...a11yProps(1)} disabled={true} />
           <Tab style={{color: "#000a12"}} label="Item Three" {...a11yProps(2)} disabled={false}/>
         </Tabs>
       </AppBar>
