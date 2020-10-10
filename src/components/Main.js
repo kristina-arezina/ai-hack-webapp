fbce44d28bab430000148f5f914efdc8889d6489
kristina-arezina <arezina.kristina23@gmail.com>
2020-10-10 14:28:52 -0400

    things work

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
