5ae6cd4afe4b7c53aaacff131c70104e522fc0a3
kristina-arezina <arezina.kristina23@gmail.com>
2020-10-14 08:58:07 -0400

    uploaded files

--- server.js
+++ server.js
@@ -14,8 +14,14 @@ app.get('/api/hello', (req, res) => {
 
 app.post('/api/world', (req, res) => {
   console.log("req.body", req.body);
-  let data = JSON.stringify(req.body);
-  fs.writeFileSync('resume-data', data);
+
+  fs.readFile('resume-data.json', (err, data) => {
+    if (err) throw err;
+    let resume = JSON.parse(data);
+    console.log(resume);
+});
+
+console.log('This is after the read call');
 
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`,
