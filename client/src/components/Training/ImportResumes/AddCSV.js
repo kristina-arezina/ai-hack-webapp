b19066774dd755c82201d651876366ea73d1fdd2
kristina-arezina <arezina.kristina23@gmail.com>
2020-10-14 00:55:58 -0400

    starting to write to file

--- client/src/components/Training/ImportResumes/AddCSV.js
+++ client/src/components/Training/ImportResumes/AddCSV.js
@@ -2,9 +2,45 @@ import React, { Component } from 'react'
 import { CSVReader } from 'react-papaparse'
 
 export default class AddCSV extends Component {
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+    data: []
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    
+    return body;
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.data }),
+    });
+    const body = await response.text();
+    
+    this.setState({ responseToPost: body });
+  };
+
   handleOnDrop = (data) => {
     console.log('---------------------------')
     console.log("data", data)
+    this.setState({ data: data })
     console.log('---------------------------')
   }
 
@@ -30,6 +66,11 @@ export default class AddCSV extends Component {
         >
           <span>Drop CSV file here or click to upload.</span>
         </CSVReader>
+        <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
       </>
     )
   }
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express Resume Json' });
@@ -13,6 +15,9 @@ app.get('/api/hello', (req, res) => {
 
 app.post('/api/world', (req, res) => {
   console.log(req.body);
+  let data = JSON.stringify(req.body);
+  fs.writeFileSync('resume-data', req.body);
+
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
