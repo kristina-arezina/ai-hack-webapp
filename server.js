2352e53e201d57b053559a6bdee15668d6e468e0
kristina-arezina <arezina.kristina23@gmail.com>
2020-10-14 08:45:04 -0400

    removed resume data from git

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,9 +13,9 @@ app.get('/api/hello', (req, res) => {
 });
 
 app.post('/api/world', (req, res) => {
-  console.log(req.body);
+  console.log("req.body", req.body);
   let data = JSON.stringify(req.body);
-  fs.writeFileSync('resume-data', req.body);
+  fs.writeFileSync('resume-data', data);
 
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`,
