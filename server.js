f70cdc3d6e1cb9bfa13fa023c198aa75d95df82b
kristina-arezina <arezina.kristina23@gmail.com>
2020-10-14 22:43:47 -0400

    cleaning up code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,15 +11,14 @@ app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express Resume Json' });
 });
 
+
 app.post('/api/world', (req, res) => {
   console.log("req.body", req.body);
 
   let data = JSON.stringify(req.body);
   fs.writeFileSync('resume-data.json', data);
 
-  
-
-console.log('This is after the read call');
+  console.log('This is after the read call');
 
   res.send(
     `We received your data, please click on the 'go to next step' button.`,
