6430790ebc5ca04f1316f691f890d7ab10f2689d
kristina-arezina <arezina.kristina23@gmail.com>
2020-10-16 17:56:43 -0400

    revovered code

--- client/src/components/Training/ImportResumes/AddCSV.js
+++ client/src/components/Training/ImportResumes/AddCSV.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { CSVReader } from 'react-papaparse'
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 export default class AddCSV extends Component {
   state = {
@@ -10,38 +11,55 @@ export default class AddCSV extends Component {
     data: []
   };
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
+  // componentDidMount() {
+  //   this.callApi()
+  //     .then(res => this.setState({ response: res.express }))
+  //     .catch(err => console.log(err));
+  // }
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
+  // callApi = async () => {
+  //   const response = await fetch('/api/hello');
+  //   const body = await response.json();
+  //   if (response.status !== 200) throw Error(body.message);
     
-    return body;
-  };
+  //   return body;
+  // };
 
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.data }),
-    });
-    const body = await response.text();
+  // handleSubmit = async e => {
+  //   e.preventDefault();
+  //   axios.post('/api/world', {
+  //     body: JSON.stringify({ post: this.state.data })
+  //   })
+  //   .then((response) => {
+  //     console.log(response);
+  //   }, (error) => {
+  //     console.log(error);
+  //   });
+  //   // const response = await fetch('/api/world', {
+  //   //   method: 'POST',
+  //   //   headers: {
+  //   //     'Content-Type': 'application/json',
+  //   //   },
+  //   //   body: JSON.stringify({ post: this.state.data }),
+  //   // });
+  //   // const body = await response.text();
     
-    this.setState({ responseToPost: body });
-  };
+  //   this.setState({ responseToPost: body });
+  // };
 
   handleOnDrop = (data) => {
     console.log('---------------------------')
     console.log("data", data)
     this.setState({ data: data })
+    axios.post('http://localhost:6001/api/world', {
+      data: this.state.data,
+    })
+    .then((response) => {
+      console.log(response);
+    }, (error) => {
+      console.log(error);
+    });
+
     console.log('---------------------------')
   }
 
