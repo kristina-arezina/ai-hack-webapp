c86fa97b8bc7d6a14220ea9332e4897a69005f5f
kristina-arezina <arezina.kristina23@gmail.com>
2020-10-27 00:55:20 -0400

    very close

--- client/src/components/FindBestCandidate/RankingList.js
+++ client/src/components/FindBestCandidate/RankingList.js
@@ -11,6 +11,7 @@ import Filter2Icon from '@material-ui/icons/Filter2';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import {items} from "../../store.js"
 
 const useStyles = makeStyles({
   root: {
@@ -32,47 +33,63 @@ const useStyles = makeStyles({
 
 export default function RankingList(props) {
   const [userResumes, setResumes] = React.useState("");
-  const [userResumesTrue, setTrueResume] = React.useState([]);
+  const [userResumesTrue, setResumesTrue] = React.useState([]);
   const classes = useStyles();
-  const resumes = {
-    one: {
-      id: 1,
+  
+  const resumes = [
+    {
+      id: "one",
+      number: 1,
       name: "Ramanpreet Kaur",
       link: "https://docs.google.com/document/d/144VFARrTOFjBkTCGzJ2oj0-BVT4akh-gJAh4Wp0fLkI/edit?usp=sharing",
     },
-    two: {
-      id: 2,
+    {
+      id: "two",
+      number: 2,
       name: "Siddu Patil",
       link: "https://docs.google.com/document/d/1CxEl3RrIEZRUMq-qa_8En4YCHml_tRyKW-vWAWi3brk/edit?usp=sharing"
     },
-    three: {
-      id: 3,
+    {
+      id: "three",
+      number: 3,
       name: "Yakeen Y. Gazi",
       link: "https://docs.google.com/document/d/17jHXruJ6gyxRoPxtfoTjg5RdSuJj6vHQljyQbxIr3g0/edit?usp=sharing"
     },
-    four: {
-      id: 3,
+    {
+      id: "four",
+      number: 4,
       name: "Mitchell Artz",
       link: "https://docs.google.com/document/d/1bdqhuPqzaLD3tfOfCLF20co4sPo2_CQdcou9OYDk-dw/edit?usp=sharing"
     }
-  };
-
+  ];
+  let newArray = [] 
+  let setResumesTrueArray = []
   useEffect(() => {
-    // Update the document title using the browser API
+    
     setResumes(props.resumes);
     console.log(userResumes.length)
-    // console.log("userResumes", userResumes[1])
+    // let newArray = [] 
+    for (var i = 0; i < userResumes.length-1; i++) {
+      let userResumesArray = userResumes[i]
+      if (userResumesArray[1] === true) {
+        console.log("userResumes", userResumesArray[0])
+        newArray.push(userResumesArray[0])
+        console.log("userResumesTrue", newArray)
+      } 
+  }
+
+  for (var i = 0; i < resumes.length-1; i++) {
+    if (newArray[i] === resumes[i].id) {
+      console.log("yes", resumes[i])
+      setResumesTrueArray.push(resumes[i])
+      console.log("setResumesTrueArray[0]", setResumesTrueArray[0].name)
+    } 
+}
   });
 
-  for (var i = 0; i < userResumes.length-1; i++) {
-    let userResumesArray = userResumes[i]
-    if (userResumesArray[1] === true) {
-      console.log("userResumes", userResumesArray[0])
-    }  
-}
+  
 
   return (
-
     <List className={classes.root}>   
     <p>{props.resumes}</p> 
     <ListItem alignItems="flex-start">
@@ -83,7 +100,7 @@ export default function RankingList(props) {
       <CardActionArea href="https://docs.google.com/document/d/144VFARrTOFjBkTCGzJ2oj0-BVT4akh-gJAh4Wp0fLkI/edit?usp=sharing" target="_blank">
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Ramanpreet Kaur
+            {setResumesTrueArray[0].name}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             Ranked as the most qualified. 
---
+++ client/src/store.js
@@ -0,0 +1,26 @@
+export const items = [
+    {
+        id: "one",
+        number: 1,
+        name: "Ramanpreet Kaur",
+        link: "https://docs.google.com/document/d/144VFARrTOFjBkTCGzJ2oj0-BVT4akh-gJAh4Wp0fLkI/edit?usp=sharing",
+    },
+    {
+        id: "two",
+        number: 2,
+        name: "Siddu Patil",
+        link: "https://docs.google.com/document/d/1CxEl3RrIEZRUMq-qa_8En4YCHml_tRyKW-vWAWi3brk/edit?usp=sharing"
+    },
+    {
+        id: "three",
+        number: 3,
+        name: "Yakeen Y. Gazi",
+        link: "https://docs.google.com/document/d/17jHXruJ6gyxRoPxtfoTjg5RdSuJj6vHQljyQbxIr3g0/edit?usp=sharing"
+    },
+    {
+        id: "four",
+        number: 4,
+        name: "Mitchell Artz",
+        link: "https://docs.google.com/document/d/1bdqhuPqzaLD3tfOfCLF20co4sPo2_CQdcou9OYDk-dw/edit?usp=sharing"
+    }
+]
